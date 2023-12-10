af72527e8c21859b462dcea460c3f829a8305364
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2023-12-10 09:46:18 +0000

    feat: add tabs, refine ui and ux

--- src/pages/DeckPage.vue
+++ src/pages/DeckPage.vue
@@ -1,22 +1,37 @@
 <template>
   <q-page>
-    <h1>Deck Page</h1>
+    <h2 class="text-center">Deck Page</h2>
     <div class="column items-center">
+      <q-tabs v-model="tab">
+        <q-tab
+          v-for="section in tabs"
+          :name="section"
+          :key="section"
+          :label="section"
+        >
+        </q-tab>
+      </q-tabs>
+
       <div class="col">
-        <QuestionCard />
+        <QuestionDeck :deck="deck" :key="tab" />
       </div>
     </div>
   </q-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import QuestionCard from 'src/components/QuestionCard.vue';
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import QuestionDeck from 'src/components/QuestionDeck.vue';
 
-export default defineComponent({
-  name: 'DeckPage',
-  components: {
-    QuestionCard,
-  },
+import domestication from 'src/assets/data/domestication.json';
+import psychology from 'src/assets/data/psychology.json';
+
+const sections = { domestication, psychology };
+
+const tabs = Object.keys(sections);
+const tab = ref(tabs[0] as keyof typeof sections);
+
+const deck = computed(() => {
+  return sections[tab.value];
 });
 </script>
