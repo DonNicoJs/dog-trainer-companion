6916e9d257cd789f63bd73c3703ee69a3d9ab548
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2023-12-10 10:04:53 +0000

    fix: remove example component

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,13 @@
 <template>
   <q-page>
     <h1>INDEX PAGE</h1>
-    <ExampleComponent />
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import ExampleComponent from 'src/components/ExampleComponent.vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { ExampleComponent },
 });
 </script>
